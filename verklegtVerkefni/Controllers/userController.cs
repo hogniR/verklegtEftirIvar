0fa8e89b9ef5793766750f464c9e163f6d4129c1
TheSolvemaster <solvihj@gmail.com>
2014-05-09 16:22:16 +0000

    let inn userController

---
+++ verklegtVerkefni/Controllers/userController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using verklegtVerkefni.Models;
+using verklegtVerkefni.RepoClasses;
+
+namespace verklegtVerkefni.Controllers
+{
+    public class userController : Controller
+    {
+        userRepository repository = new userRepository();
+        public ActionResult logIn()
+        {
+
+            return View();
+        }
+        public ActionResult register()
+        {
+            return View(new users());
+        }
+        [HttpPost]
+        public ActionResult register(FormCollection form)
+        {
+            users newItem = new users();
+            UpdateModel(newItem);
+            repository.addNewUser(newItem);
+            repository.save();
+            return RedirectToAction("Index", "Home");
+        }
+	}
+}
