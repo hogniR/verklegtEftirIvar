0eaea5bb9901582747fb909b374fee7c3acd4c6a
TheSolvemaster <solvihj@gmail.com>
2014-05-09 16:25:16 +0000

    bætti við userRepo

---
+++ verklegtVerkefni/RepoClasses/userRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using verklegtVerkefni.Models;
+
+namespace verklegtVerkefni.RepoClasses
+{
+    public class userRepository
+    {
+        DataContext m_db = new DataContext();
+
+        public void addNewUser(users newUser)
+        {
+            m_db.users.Add(newUser);
+            save();
+            return;
+        }
+        public void save()
+        {
+            m_db.SaveChanges();
+            return;
+        }
+    }
+}
