6eeb74d88c4b0ec7470a45459a1ecf12fdc47e3f
hogniR <hogni@ingimarsson.com>
2014-05-12 16:45:16 +0000

    upload komid nema ekki content - sida buildast

---
+++ verklegtVerkefni/Models/files.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace verklegtVerkefni.Models
+{
+    public class files
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int series { get; set; }
+        public int episode { get; set; }
+        public string language { get; set; }
+        public string content { get; set; }
+        public int likes { get; set; }
+    }
+}
---
+++ verklegtVerkefni/RepoClasses/filesRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using verklegtVerkefni.Models;
+
+namespace verklegtVerkefni.RepoClasses
+{
+    public class filesRepository
+    {
+        DataContext m_db = new DataContext();
+        public void addNewFile(files newFile)
+        {
+            m_db.files.Add(newFile);
+            save();
+            return;
+        }
+        public files getFileById(int id)
+        {
+            var result = (from s in m_db.files
+                          where s.id == id
+                          select s).SingleOrDefault();
+
+            return result;
+        }
+        public void save()
+        {
+            m_db.SaveChanges();
+            return;
+        }
+    }
+}
